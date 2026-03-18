d24030be4e70578f0f6d7fb34bb46ee0fb5098a7
Xantos07 <coalstudio7@gmail.com>
2026-03-18 18:46:32 +0100

    🎉init project

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+
+
+def main():
+    print("lancement du processus ETL...")
+    # extraction
+    # transformation
+    # chargement
+    while True:
+        # Cycle à préparer
+        time.sleep(60)  
+
+if __name__ == "__main__":
+    main()
